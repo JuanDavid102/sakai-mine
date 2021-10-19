f8665733038b686ba4d8c9d193f59647c23a4d01
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2021-10-19 11:48:39 +0200

    Grade Sync Job: Ability to disable the job and better configuration.

--- src/main/java/edu/uc/ltigradebook/job/SyncCanvasGradesJob.java
+++ src/main/java/edu/uc/ltigradebook/job/SyncCanvasGradesJob.java
@@ -1,7 +1,9 @@
 package edu.uc.ltigradebook.job;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,9 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class SyncCanvasGradesJob {
 
+    @Value("${sync.grades.enabled:false}")
+    private boolean enableGradeSyncJob;
+
     @Autowired
     private CourseRepository courseRepository;
 
@@ -26,15 +31,24 @@ public class SyncCanvasGradesJob {
 
     @Scheduled(fixedDelayString = "${sync.grades.interval}", initialDelayString = "${sync.grades.initial.delay}")
     public void run() {
+        if (!enableGradeSyncJob) {
+            return;
+        }
         log.info("Running Canvas Grades Synchronization process....");
+        StopWatch stopwatch = StopWatch.createStarted();
+        // Explores the courses present in the repository, basically synchronizes the courses present in the platform.
         courseRepository.findAll().forEach(course -> {
             String courseId = String.valueOf(course.getCourseId());
             try {
+                // This trick is a courseId checker, it fails if the course does not exist in the Canvas instance, ideally when mixing instances or when a course is deleted.
                 canvasService.getSingleCourse(courseId);
+                // For each course present in the system, gets the assignments using the API.
                 canvasService.listCourseAssignments(courseId).forEach(assignment -> {
                     Integer assignmentId = assignment.getId();
                     try {
+                        // For each assignment present in the course, gets the course submissions.
                         canvasService.getCourseSubmissions(courseId, assignmentId).forEach(assignmentSubmission -> {
+                            // For each grade present in the submission, stores the grade in the local DB.
                             String userId = assignmentSubmission.getUserId().toString();
                             String grade = assignmentSubmission.getGrade();
                             log.debug("Dumping grades from submission {}, user {}, assignment {} and course {}, grade is {}.", assignmentSubmission.getId(), userId, assignmentId, courseId, grade);
@@ -55,7 +69,7 @@ public class SyncCanvasGradesJob {
                 log.error("Fatal error getting course {}, skipping. {}", courseId, e.getMessage());
             }
         });
-        log.info("The Canvas Grades Synchronization process has ended and dumped all the canvas grades to the local DB.");
-        
+        stopwatch.stop();
+        log.info("The Canvas Grades Synchronization process has been completed in {}.", stopwatch);        
     }
 }
