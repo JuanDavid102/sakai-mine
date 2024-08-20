sakaiproject/sakai

S2U-26 Gradebook: support for group instances

gradebookng/api/src/main/java/org/sakaiproject/grading/api/GradingAuthz.java
@@ -34,50 +34,54 @@ public interface GradingAuthz {
         PERMISSION_VIEW_OWN_GRADES = "gradebook.viewOwnGrades",
         PERMISSION_VIEW_STUDENT_NUMBERS = "gradebook.viewStudentNumbers";
 
-    public boolean isUserAbleToGrade(String gradebookUid);
-    public boolean isUserAbleToGrade(String gradebookUid, String userUid);
-    public boolean isUserAbleToGradeAll(String gradebookUid);
-    public boolean isUserAbleToGradeAll(String gradebookUid, String userUid);
-    public boolean isUserAbleToEditAssessments(String gradebookUid);
-    public boolean isUserAbleToViewOwnGrades(String gradebookUid);
-    public boolean isUserAbleToViewStudentNumbers(String gradebookUid);
+    public boolean isUserAbleToGrade(String siteId);
+    public boolean isUserAbleToGrade(String siteId, String userUid);
+    public boolean isUserAbleToGradeAll(String siteId);
+    public boolean isUserAbleToGradeAll(String siteId, String userUid);
+    public boolean isUserAbleToEditAssessments(String siteId);
+    public boolean isUserAbleToViewOwnGrades(String siteId);
+    public boolean isUserAbleToViewStudentNumbers(String siteId);
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @param itemId
      * @param studentUid
      * @return is user authorized to grade this gradebook item for this student?
      *      first checks for special grader perms. if none, uses default perms
      */
-    public boolean isUserAbleToGradeItemForStudent(String gradebookUid, Long itemId, String studentUid)  throws IllegalArgumentException;
+    public boolean isUserAbleToGradeItemForStudent(String gradebookUid, String siteId, Long itemId, String studentUid)  throws IllegalArgumentException;
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @param itemId
      * @param studentUid
      * @return is user authorized to view this gradebook item for this student?
      *      first checks for special grader perms. if none, uses default perms
      */
-    public boolean isUserAbleToViewItemForStudent(String gradebookUid, Long itemId, String studentUid)  throws IllegalArgumentException;
+    public boolean isUserAbleToViewItemForStudent(String gradebookUid, String siteId, Long itemId, String studentUid)  throws IllegalArgumentException;
 
     /**
-     * @param gradebookUid
+     * @param siteId
      * @return all of the CourseSections for this site
      */
-    public List<CourseSection> getAllSections(String gradebookUid);
+    public List<CourseSection> getAllSections(String siteId);
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @return all CourseSections that the current user may view or grade
      */
-    public List<CourseSection> getViewableSections(String gradebookUid);
+    public List<CourseSection> getViewableSections(String gradebookUid, String siteId);
 
     /**
-     * @param gradebookUid
      * @param userUid
+     * @param gradebookUid
+	 * @param siteId
      * @param categoryId
      *          The category id that the desired item is associated with
      * @param gbCategoryType
@@ -90,11 +94,12 @@ public interface GradingAuthz {
      * @return a map of EnrollmentRecords to grade/view permission that the given user is authorized to
      *          view or grade for the given gradebook item
      */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItemForUser(String userUid, String gradebookUid, Long categoryId, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
+    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItemForUser(String userUid, String gradebookUid, String siteId, Long categoryId, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
 
     /**
      *
      * @param gradebookUid
+	 * @param siteId
      * @param categoryId
      *          The category id that the desired item is associated with
      * @param gbCategoryType
@@ -107,11 +112,12 @@ public interface GradingAuthz {
      * @return a map of EnrollmentRecords to grade/view permission that the current user is authorized to
      *          view or grade for the given gradebook item
      */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItem(String gradebookUid, Long categoryId, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
+    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItem(String gradebookUid, String siteId, Long categoryId, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
 
     /**
      *
      * @param gradebookUid
+	 * @param siteId
      * @param gbCategoryType
      *          The category type setting for this gradebook
      * @param optionalSearchString
@@ -122,21 +128,7 @@ public interface GradingAuthz {
      *          everything in the gradebook for that student. If he/she can grade only a subset of the items, the
      *          student is not returned.
      */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForViewableCourseGrade(String gradebookUid, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
-    /**
-     *
-     * @param gradebookUid
-     * @param allGbItems
-     *          List of all Assignments associated with this gradebook
-     * @param optionalSearchString
-     *          a substring search for student name or display UID; the exact rules are
-     *          up to the implementation - leave null to use all students
-     * @param optionalSectionUid
-     *          null if the search should be made across all sections
-     * @return a map of EnrollmentRecords to a map of item id and function (grade/view) that the user is
-     *          authorized to view/grade
-     */
-    public Map<EnrollmentRecord, Map<Long, String>> findMatchingEnrollmentsForViewableItems(String gradebookUid, List allGbItems, String optionalSearchString, String optionalSectionUid);
+    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForViewableCourseGrade(String gradebookUid, String siteId, GradingCategoryType gbCategoryType, String optionalSearchString, String optionalSectionUid);
 
     /**
      * Check to see if current user may grade or view the given student for the given item in the given gradebook.

JuanDavid102/sakai-mine

Co-authored-by: jumarub <juanmanuelrubialesdelacruz@gmail.com>
Co-authored-by: JuanDavid102 <jmassanet@entornosdeformacion.com>

gradebookng/api/src/main/java/org/sakaiproject/grading/api/GradingAuthz.java
@@ -34,50 +34,54 @@ public interface GradingAuthz {
         PERMISSION_VIEW_OWN_GRADES = "gradebook.viewOwnGrades",
         PERMISSION_VIEW_STUDENT_NUMBERS = "gradebook.viewStudentNumbers";
 
-    public boolean isUserAbleToGrade(String gradebookUid);
-    public boolean isUserAbleToGrade(String gradebookUid, String userUid);
-    public boolean isUserAbleToGradeAll(String gradebookUid);
-    public boolean isUserAbleToGradeAll(String gradebookUid, String userUid);
-    public boolean isUserAbleToEditAssessments(String gradebookUid);
-    public boolean isUserAbleToViewOwnGrades(String gradebookUid);
-    public boolean isUserAbleToViewStudentNumbers(String gradebookUid);
+    public boolean isUserAbleToGrade(String siteId);
+    public boolean isUserAbleToGrade(String siteId, String userUid);
+    public boolean isUserAbleToGradeAll(String siteId);
+    public boolean isUserAbleToGradeAll(String siteId, String userUid);
+    public boolean isUserAbleToEditAssessments(String siteId);
+    public boolean isUserAbleToViewOwnGrades(String siteId);
+    public boolean isUserAbleToViewStudentNumbers(String siteId);
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @param itemId
      * @param studentUid
      * @return is user authorized to grade this gradebook item for this student?
      *      first checks for special grader perms. if none, uses default perms
      */
-    public boolean isUserAbleToGradeItemForStudent(String gradebookUid, Long itemId, String studentUid)  throws IllegalArgumentException;
+    public boolean isUserAbleToGradeItemForStudent(String gradebookUid, String siteId, Long itemId, String studentUid)  throws IllegalArgumentException;
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @param itemId
      * @param studentUid
      * @return is user authorized to view this gradebook item for this student?
      *      first checks for special grader perms. if none, uses default perms
      */
-    public boolean isUserAbleToViewItemForStudent(String gradebookUid, Long itemId, String studentUid)  throws IllegalArgumentException;
+    public boolean isUserAbleToViewItemForStudent(String gradebookUid, String siteId, Long itemId, String studentUid)  throws IllegalArgumentException;
 
     /**
-     * @param gradebookUid
+     * @param siteId
      * @return all of the CourseSections for this site
      */
-    public List<CourseSection> getAllSections(String gradebookUid);
+    public List<CourseSection> getAllSections(String siteId);
 
     /**
      *
      * @param gradebookUid
+     * @param siteId
      * @return all CourseSections that the current user may view or grade
      */
-    public List<CourseSection> getViewableSections(String gradebookUid);
+    public List<CourseSection> getViewableSections(String gradebookUid, String siteId);
 
     /**
-     * @param gradebookUid
      * @param userUid
+     * @param gradebookUid
+	 * @param siteId
      * @param categoryId
      *          The category id that the desired item is associated with
      * @param gbCategoryType
@@ -90,11 +94,12 @@ public interface GradingAuthz {
      * @return a map of EnrollmentRecords to grade/view permission that the given user is authorized to
      *          view or grade for the given gradebook item
      */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItemForUser(String userUid, String gradebookUid, Long categoryId, Integer gbCategoryType, String optionalSearchString, String optionalSectionUid);
+    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItemForUser(String userUid, String gradebookUid, String siteId, Long categoryId, Integer gbCategoryType, String optionalSearchString, String optionalSectionUid);
 
     /**
      *
      * @param gradebookUid
+	 * @param siteId
      * @param categoryId
      *          The category id that the desired item is associated with
      * @param gbCategoryType
@@ -107,44 +112,16 @@ public interface GradingAuthz {
      * @return a map of EnrollmentRecords to grade/view permission that the current user is authorized to
      *          view or grade for the given gradebook item
      */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItem(String gradebookUid, Long categoryId, Integer gbCategoryType, String optionalSearchString, String optionalSectionUid);
-
-    /**
-     *
-     * @param gradebookUid
-     * @param gbCategoryType
-     *          The category type setting for this gradebook
-     * @param optionalSearchString
-     * @param optionalSectionUid
-     * @return Map of EnrollmentRecord --> function (grade/view) for all students that the current user has permission to
-     *          view/grade every item in the gradebook. If he/she can grade everything, GRADE function is
-     *          returned. Otherwise, function is VIEW. May only modify course grade if he/she can grade
-     *          everything in the gradebook for that student. If he/she can grade only a subset of the items, the
-     *          student is not returned.
-     */
-    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForViewableCourseGrade(String gradebookUid, Integer gbCategoryType, String optionalSearchString, String optionalSectionUid);
-    /**
-     *
-     * @param gradebookUid
-     * @param allGbItems
-     *          List of all Assignments associated with this gradebook
-     * @param optionalSearchString
-     *          a substring search for student name or display UID; the exact rules are
-     *          up to the implementation - leave null to use all students
-     * @param optionalSectionUid
-     *          null if the search should be made across all sections
-     * @return a map of EnrollmentRecords to a map of item id and function (grade/view) that the user is
-     *          authorized to view/grade
-     */
-    public Map<EnrollmentRecord, Map<Long, String>> findMatchingEnrollmentsForViewableItems(String gradebookUid, List allGbItems, String optionalSearchString, String optionalSectionUid);
+    public Map<EnrollmentRecord, String> findMatchingEnrollmentsForItem(String gradebookUid, String siteId, Long categoryId, Integer gbCategoryType, String optionalSearchString, String optionalSectionUid);
 
     /**
      * Check to see if current user may grade or view the given student for the given item in the given gradebook.
      * Returns string representation of function per GradingService vars (view/grade) or null if no permission
      * @param gradebookUid
+     * @param siteId
      * @param itemId
      * @param studentUid
      * @return GradingService.gradePermission, GradingService.viewPermission, or null if no permission
      */
-    public String getGradeViewFunctionForUserForStudentForItem(String gradebookUid, Long itemId, String studentUid);
+    public String getGradeViewFunctionForUserForStudentForItem(String gradebookUid, String siteId, Long itemId, String studentUid);
 }